e90a33f1242e1345d7749bdc92d114c47f8d36b7
Noah Douglas <douglas.noah7344@gmail.com>
2018-10-06 14:44:30 -0500

    App Update
    Now prints lyrics after an artist and song is entered

--- app/app.R
+++ app/app.R
@@ -1,37 +1,40 @@
 library(shiny)
-
+source("Dataset.r")
 ui <- fluidPage(
-  titlePanel("censusVis"),
+  titlePanel("Song Sentiment Analysis"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Create demographic maps with 
-               information from the 2010 US Census."),
+      helpText("Let Us Help You Find Your New Favorite Song"),
       
       selectInput("var", 
                   label = "Choose a variable to display",
                   choices = c("Happy","Sad","Love","Dance"),
                   selected = "Dance"),
-      
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
+      textInput("band",
+                label = "Enter a Band or Artist Name",
+                placeholder="Band Name"),
+      textInput("song",
+                label = "Enter a song Title",
+                placeholder = "Song Title"),
+      submitButton(text= "Generate Info")
     ),
-    
     mainPanel(
-      textOutput("selected_var")
+      textOutput("selected_var"),
+      textOutput("selected_var_2")
     )
   )
- )
-
-
-
+)
 #Define server logic to plot various variables against mpg
 
 server <- function(input,output){
   output$selected_var <- renderText({ 
-    paste("You have selected", input$var)
+    paste("You Searched for the Artist ", input$band)
   })
+  output$selected_var_2 <- renderText({
+    paste("You Searched for the Song", input$song)
+  })
+  scrapeLyrics()
 }
 
 shinyApp(ui,server)
