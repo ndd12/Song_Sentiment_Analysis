2a49c74bf0a925829991c1d45e1fa2347bfcbca3
Noah Douglas <douglas.noah7344@gmail.com>
2018-10-06 18:24:07 -0500

    Update App folder
    Added Bar Plots

--- app/app.R
+++ app/app.R
@@ -1,40 +1,47 @@
 library(shiny)
-source("Dataset.r")
+library(reticulate)
+source("webScrape/webScrape.R")
+source_python("lyricProccessing/processLyrics.py")
+py_available()
+
+
 ui <- fluidPage(
   titlePanel("Song Sentiment Analysis"),
   
   sidebarLayout(
     sidebarPanel(
       helpText("Let Us Help You Find Your New Favorite Song"),
-      
-      selectInput("var", 
-                  label = "Choose a variable to display",
-                  choices = c("Happy","Sad","Love","Dance"),
-                  selected = "Dance"),
       textInput("band",
                 label = "Enter a Band or Artist Name",
                 placeholder="Band Name"),
       textInput("song",
                 label = "Enter a song Title",
                 placeholder = "Song Title"),
+      sliderInput("happiness",
+                  label = "Happiness",
+                  min=0, max=10, value=5
+                  ),
+      sliderInput("sadness",
+                  label = "Sadness",
+                  min=0, max=10, value=5),
       submitButton(text= "Generate Info")
     ),
     mainPanel(
-      textOutput("selected_var"),
-      textOutput("selected_var_2")
+      textOutput('selected_var_3'),
+      plotOutput("distPlot")
     )
   )
 )
 #Define server logic to plot various variables against mpg
 
 server <- function(input,output){
-  output$selected_var <- renderText({ 
-    paste("You Searched for the Artist ", input$band)
+  output$selected_var_3 <- renderText({
+    paste(scrapeLyrics(input$band,input$song))
   })
-  output$selected_var_2 <- renderText({
-    paste("You Searched for the Song", input$song)
+  output$distPlot <- renderPlot({
+    data <- c(input$happiness , input$sadness)
+    barplot(data, xlab="Happiness")
   })
-  scrapeLyrics()
 }
-
+scrapeLyrics(input$band,input$song)
 shinyApp(ui,server)
