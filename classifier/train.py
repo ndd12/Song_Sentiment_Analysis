9bafe3b539a126f15df3d0a507a8dc40c8767e32
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-10-06 23:34:06 -0500

    update

--- classifier/train.py
+++ classifier/train.py
@@ -1,12 +1,25 @@
 from sklearn.linear_model import LogisticRegression
-from processLyrics import process
+from preprocessLyrics import process
 from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
 
 happyFile = 'trainingData/happy_lyrics.txt'
 sadFile = 'trainingData/sad_lyrics.txt'
-X,Y = process(happyFile, sadFile)
+
+happySongsLyrics = open(happyFile, encoding="utf8").readlines()
+sadSongsLyrics = open(sadFile, encoding="utf8").readlines()
+
+Y = ["happy"] * len(happySongsLyrics) + ["sad"] * len(sadSongsLyrics)
+allSongsLyrics = happySongsLyrics + sadSongsLyrics
+processed_allSongsLyrics = process(allSongsLyrics)
+
+matrix = CountVectorizer(max_features=1000)
+X = matrix.fit_transform(processed_allSongsLyrics).toarray()
 
 logisticRegr = LogisticRegression()
 logisticRegr.fit(X, Y)
 
-joblib.dump(logisticRegr, "model.joblib")
+pipeline = Pipeline([('vectorizer', matrix), ('model', logisticRegr)])
+
+joblib.dump(pipeline, "pipeline.joblib")
