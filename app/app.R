18a9c4abeffbebe82b85530d6a6ea13fa239209e
Spencer Gritton <SpencerGritton@wireless-nat-inside.uiowa.edu>
2018-10-06 22:46:24 -0500

    routes

--- app/app.R
+++ app/app.R
@@ -1,10 +1,20 @@
 library(shiny)
+library(shiny.router)
 library(reticulate)
 source("webScrape/webScrape.R")
 source_python("classifier/test.py")
 
-ui <- fluidPage(
-  titlePanel("Song Sentiment Analysis"),
+string1 <- scrapeLyrics(input$band,input$song)
+strsplit(string1," ")[[1]]
+shinyApp(ui,server)
+
+# This creates UI for each page.
+root_page <- function(title, content) {
+  div(
+    titlePanel(title),
+    p(content)
+  )
+  titlePanel("Song Sentiment Analysis")
   
   sidebarLayout(
     sidebarPanel(
@@ -18,7 +28,7 @@ ui <- fluidPage(
       sliderInput("happiness",
                   label = "Happiness",
                   min=0, max=10, value=5
-                  ),
+      ),
       submitButton(text= "Generate Info")
     ),
     mainPanel(
@@ -26,9 +36,46 @@ ui <- fluidPage(
       plotOutput("distPlot")
     )
   )
+}
+
+# This creates UI for each page.
+spotify_page <- function(title, content) {
+  div(
+    titlePanel(title),
+    p(content)
+  )
+}
+
+# Both sample pages.
+root_page <- root_page("Home page", "Welcome on sample routing page!")
+spotify_page <- spotify_page("Some spotify page", "spotify")
+
+# Callbacks on the server side for
+# the pages
+root_callback <- function(input, output, session) {
+  # yeet
+}
+
+spotify_callback <- function(input, output, session) {
+  #yeet
+}
+
+# Creates router. We provide routing path, a UI as
+# well as a server-side callback for each page.
+router <- make_router(
+  route("/", root_page, root_callback),
+  route("spotify", spotify_page, spotify_callback)
 )
 
-server <- function(input,output){
+# Creat output for our router in main UI of Shiny app.
+ui <- shinyUI(fluidPage(
+  router_ui()
+))
+
+# Plug router into Shiny server.
+server <- shinyServer(function(input, output, session) {
+  router(input, output, session)
+  
   output$selected_var_3 <- renderText({
     string1 <- (scrapeLyrics(input$band,input$song))
     paste(test(scrapeLyrics(input$band,input$song)))
@@ -39,9 +86,9 @@ server <- function(input,output){
             col=c("yellow","blue"),
             xlab = "Emotion", names = c("Happiness", "Sadness"),
             horiz = TRUE
-            )
+    )
   })
-}
-string1 <- scrapeLyrics(input$band,input$song)
-strsplit(string1," ")[[1]]
-shinyApp(ui,server)
+})
+
+# Run server in a standard way.
+shinyApp(ui, server)
