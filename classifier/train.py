3579124a0bc8120b337318e3a6cf6e76b1409bf3
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-10-07 10:14:31 -0500

    cross-validate model and use Niave Bayes

--- classifier/train.py
+++ classifier/train.py
@@ -1,8 +1,9 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from processLyrics import process
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
 happyFile = 'trainingData/happy_lyrics.txt'
 sadFile = 'trainingData/sad_lyrics.txt'
@@ -17,8 +18,13 @@ processed_allSongsLyrics = process(allSongsLyrics)
 vectorizer = CountVectorizer(max_features=1000)
 X = vectorizer.fit_transform(processed_allSongsLyrics).toarray()
 
-classifier = LogisticRegression()
-classifier.fit(X, Y)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+predictions = classifier.predict(X_test)
+print("Score: " , classifier.score(X_test, y_test))
 
 joblib.dump(vectorizer, 'vectorizer.pk1')
 joblib.dump(classifier, 'classifier.pk1')
